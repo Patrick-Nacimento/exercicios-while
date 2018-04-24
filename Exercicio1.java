7f4b5f8bf32e8405bf044754a9386fc9d6deb387
Patrick-Nacimento <38558599+Patrick-Nacimento@users.noreply.github.com>
2018-04-24 11:29:56 -0300

    continuando exercicios

--- Exercicio1.java
+++ Exercicio1.java
@@ -2,6 +2,15 @@ import javax.swing.JOptionPane;
 
 public class Exercicio1{
     public static void main(String[] args){
-        
+
+        String nome = JOptionPane.showInputDialog("Digite seu nome");
+
+        while(!nome.equalsIgnoreCase("fim")){
+
+            nome = JOptionPane.showInputDialog("Digite seu nome. \nSe desejar sair \"fim\". ");
+
+            System.out.print("Sair ou continuar");
+        }
+
     }
 }
